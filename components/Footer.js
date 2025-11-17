e162de8a80d91198ba768fcb10f07e1f01d3a279
NKT1204 <nithya.exd@gmail.com>
2025-11-17 16:25:54 +0530

    Minor UI tweak

--- components/Footer.js
+++ components/Footer.js
@@ -7,15 +7,15 @@ export default function Footer() {
         <Ctnr className="py-12">
             <div className="flex flex-row gap-2">
                 <div className="w-1/2 flex flex-col gap-2">
-                    <div className="p-8 bg-[var(--color-tertiary)] text-white rounded-xl">
+                    <div className="p-5 sm:p-8 bg-[var(--color-tertiary)] text-white rounded-xl">
                         <Image src="/images/yi-logo-dark-bg.avif" alt="Yukti Insights Logo" className="w-48" width={900} height={500} />
                     </div>
-                    <div className="p-8 bg-[var(--color-tertiary)] text-white rounded-xl flex flex-col justify-end flex-1">
+                    <div className="p-5 sm:p-8 bg-[var(--color-tertiary)] text-white rounded-xl flex flex-col justify-end flex-1">
                         <p>&copy; {new Date().getFullYear()} Yukti Insights</p>
                     </div>
                 </div>
                 <div className="w-1/2 flex flex-col gap-2">
-                    <div className="p-8 bg-[var(--color-tertiary)] text-white rounded-xl flex flex-col gap-2">
+                    <div className="p-5 sm:p-8 bg-[var(--color-tertiary)] text-white rounded-xl flex flex-col gap-2">
                         <h3 className="text-lg instrument-serif-regular opacity-75">COMPANY</h3>
                         <ul className="flex flex-col">
                             <li>
@@ -45,7 +45,7 @@ export default function Footer() {
                             </li>
                         </ul>
                     </div>
-                    <div className="p-8 bg-[var(--color-tertiary)] text-white rounded-xl flex flex-col gap-2">
+                    <div className="p-5 sm:p-8 bg-[var(--color-tertiary)] text-white rounded-xl flex flex-col gap-2">
                         <h3 className="text-lg instrument-serif-regular opacity-75">CONNECT</h3>
                         <ul className="flex flex-col">
                             <li>
@@ -60,7 +60,7 @@ export default function Footer() {
                             </li>
                         </ul>
                     </div>
-                    <div className="p-8 bg-[var(--color-tertiary)] text-white rounded-xl">
+                    <div className="p-5 sm:p-8 bg-[var(--color-tertiary)] text-white rounded-xl">
                         <p>Built with 💛 in Bengaluru, India—560065</p>
                     </div>
                 </div>
